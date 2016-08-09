33f9561a5faae5c932ad0394bd9ab4bbf86c10c1
Senthil Ramasamy <www.senthilr.com@gmail.com>
2016-08-09 10:58:27 +0530

    initial commit

---
+++ Investigate.R
@@ -0,0 +1,44 @@
+library(ggplot2)
+library(plyr)
+library(RColorBrewer)
+library(grid)
+
+#load single genre file
+mlDat <- read.csv("Results/unifiedMLData.csv")
+
+#fix dates field
+mlDat$release_date <- as.Date(mlDat$release_date, "%Y-%m-%d")
+
+#sanity checking
+str(mlDat)
+
+summary(mlDat)
+
+head(mlDat)
+tail(mlDat)
+
+#load multi genre file
+mlDat_multi <- read.csv("Results/unifiedMLDataMulti.csv")
+
+#fix dates field
+mlDat_multi$release_date <- as.Date(mlDat_multi$release_date, "%Y-%m-%d")
+
+#sanity checking
+
+
+str(mlDat_multi)
+summary(mlDat_multi)
+head(mlDat_multi)
+tail(mlDat_multi)
+
+
+#prepare table for analysis of users
+
+mlDat_user <- ddply(mlDat, ~user_id + age + gender + occupation, summarize, mean_rating = mean(rating))
+agePlot <- ggplot(mlDat_user, aes(age)) + geom_histogram(aes(y=..density..), binwidth=1, colour="black", fill="white")
+
+agePlot <- agePlot + geom_density(alpha=.2, fill="#FF6666")
+
+print(agePlot)
+
+ggsave(filename = "agePlot.pdf")
